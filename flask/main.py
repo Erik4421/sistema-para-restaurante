e7c3fdbca805104ab04ee41e093cbf3bdc17fde0
Erik Silva <oliveiraerik464@gmail.com>
2025-02-19 15:58:04 -0300

    First Commit

---
+++ flask/main.py
@@ -0,0 +1,12 @@
+from flask import *
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+app.run(debug=True)
